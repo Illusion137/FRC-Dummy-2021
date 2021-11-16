fab3bcbb04f49c5a4097aa403bcab00068f8def7
Daniel Raygoza <68447327+Primal-Illusion@users.noreply.github.com>
2021-11-16 15:41:12 -0700

    Co-authored-by: BadGamesInc <BadGamesInc@users.noreply.github.com>

---
+++ src/main/cpp/RobotContainer.cpp
@@ -0,0 +1,1 @@
+#include "RobotContainer.h"
---
+++ src/main/include/RobotContainer.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <frc2/command/Command.h>
+namespace NERDS
+{
+    class RobotContainer
+    {
+    public:
+        RobotContainer();
+    private:
+        /* All subsystem member varibles */
+    };
+}
